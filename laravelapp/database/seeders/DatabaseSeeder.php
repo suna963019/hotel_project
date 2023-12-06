bb4cf6ab8abb98f917ce1752b2817205eb7dd650
eiko77 <eiko719chaba@gmail.com>
2023-12-06 10:03:07 +0900

    20231206_1 nakagawa

--- laravelapp/database/seeders/DatabaseSeeder.php
+++ laravelapp/database/seeders/DatabaseSeeder.php
@@ -12,11 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //通常処理＆Faker処理
+        $this->call(GuestsTableSeeder::class);
+     
     }
 }
