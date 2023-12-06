7121979b5a1110f16d44bc124596779410f04e12
subaaaaaaa <suba922.sk19@icloud.com>
2023-12-06 13:11:23 +0900

    guests_allergiesテーブルのマイグレーションファイルとシーダの作成

---
+++ laravelapp/database/seeders/Guests_allergiesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Guests_allergiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 1; $i < 6; $i++) {
+            $param = [
+                'guest_id' => $i,
+                'allergy_id' => $i,
+            ];
+            DB::table('guests_allergies')->insert($param);
+        }
+    }
+}
