df57b2c0e2e84736ec608e6fc12977b32af34fac
airi0804 <apple201701abc@icloud.com>
2023-12-11 16:44:40 +0900

    test

--- laravelapp/app/Http/Controllers/CourseController.php
+++ laravelapp/app/Http/Controllers/CourseController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class CourseController extends Controller
 {
-    //
+    $items = Course::all();
+    return view('course.index', ['items' => $items]);
 }
