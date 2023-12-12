79cd7b224163775ae9d54447eda85b047ee39552
suna963019 <sunahara20011205@gmail.com>
2023-12-12 15:53:44 +0900

    修正

--- laravelapp/resources/views/reserve/index.blade.php
+++ laravelapp/resources/views/reserve/index.blade.php
@@ -3,18 +3,24 @@
 @section('title', 'reserve.index')
 
 @section('content')
-    <table class="table">
-        <tr>
-            <th>コース名</th>
-            <th>人数</th>
-            <th>予約日時</th>
-        </tr>
-        @foreach ($items as $item)
+    <div>
+        <h2><a href="/reserve/del">予約の削除ページへ移動</a></h2>
+        <br>
+        <table class="table">
             <tr>
-                <td>{{ $item->course->name }}</td>
-                <td>{{ $item->number }}</td>
-                <td>{{ $item->datetime }}</td>
+                <th>コース名</th>
+                <th>人数</th>
+                <th>予約日時</th>
             </tr>
-        @endforeach
-    </table>
+            @foreach ($items as $item)
+                <tr>
+                    <td>{{ $item->course->name }}</td>
+                    <td>{{ $item->number }}</td>
+                    <td>{{ $item->datetime }}</td>
+
+                </tr>
+            @endforeach
+        </table>
+    </div>
+
 @endsection
